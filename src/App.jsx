d50f899fc61eae7b24325d03c8498c5079ad4951
Ricky Prahatama <ricky.prahatama@gmail.com>
2020-01-11 13:46:58 +0700

    Added main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Layout, Menu } from "antd";
 import "./App.css";
 import HomeScreen from "./screen/home";
 
 function App() {
-  return <HomeScreen />;
+  return (
+    <Layout>
+      <Layout.Header style={{ position: "sticky", top: 0, zIndex: 10 }}>
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["home"]}
+          style={{ lineHeight: "64px" }}
+        >
+          <Menu.Item key="home">Home</Menu.Item>
+          {/* <Menu.Item key="about">About</Menu.Item> */}
+        </Menu>
+      </Layout.Header>
+      <Layout.Content style={{ padding: "50px" }}>
+        <HomeScreen />
+      </Layout.Content>
+      <Layout.Footer style={{ textAlign: "center" }}>
+        Pokedex Sample
+      </Layout.Footer>
+    </Layout>
+  );
 }
 
 export default App;
